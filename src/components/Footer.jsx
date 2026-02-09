9b5e4b8f084c5840b47a40dc08dd55b0c3360784
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-09 20:21:14 +0530

    Initial project setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit reprehenderit odit excepturi. Illo inventore alias ut, dolores nulla fugit consequatur aut odit iure corporis eligendi perferendis, ea qui, quas consectetur?
+            </p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-312-446-7890</li>
+                <li>contact@foreveryone.com</li>
+            </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2026@ forever.com - All Right Reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
