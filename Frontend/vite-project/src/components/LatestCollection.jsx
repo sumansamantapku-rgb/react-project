724cd952a13d2947960b8233604c89e3a835ba1c
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-11 18:18:28 +0530

    Initial ecommerce frontend and backend setup

---
+++ Frontend/vite-project/src/components/LatestCollection.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useContext } from 'react'
+import Title from './Title';
+import { useState} from 'react';
+import { useEffect} from 'react';
+import Productitem from './Productitem';
+
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
+
+
+    useEffect(()=>{
+      setLatestProducts(products.slice(0,10));
+    },[])
+    
+
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTION'}/>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nemo vero. Repellendus nihil eligendi eius vitae eos sint, placeat perspiciatis cupiditate dignissimos praesentium. Autem fugiat temporibus aspernatur accusantium ea numquam?
+        </p>
+
+      </div>
+
+      {/* Rendering Products */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index)=>(
+            <Productitem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
