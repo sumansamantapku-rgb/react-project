f69969f47514d59caa2b724b004c3765d7a1d45b
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-12 18:15:56 +0530

    added backend logic for login and sign in

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,20 @@
+
+ // Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user register
+
+const registerUser = async (req, res) => {
+
+}
+
+// Route for admin login
+const adminLogin = async (req, res)=> {
+
+}
+
+
+
+export { loginUser, registerUser, adminLogin}
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoute.js'
+import connectCloudinary from './config/cloudinary.js'
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res)=>{
     res.send("API Working")
