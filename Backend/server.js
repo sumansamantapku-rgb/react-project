de92b5430d3d12ab88b02992cb96d09328242268
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-12 19:32:22 +0530

    added backend logic for login and sign in

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
-import userRouter from './routes/userRoute.js'
+import userRouter from './routes/userRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 // App Config
 const app = express()
