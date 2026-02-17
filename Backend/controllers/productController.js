4ff7fcb804f8a9471bb32a20dc8caea099e4d7e7
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-17 21:03:00 +0530

    added features so that admin can add products on website

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,4 +1,5 @@
-
+import {v2 as cloudinary} from "cloudinary"
+import productModel from "../models/productModel.js"
 
 // fuction for add product
 const addProduct = async (req, res) => {
@@ -6,15 +7,36 @@ const addProduct = async (req, res) => {
     try {
         const {name, description, price, category, subCategory, sizes, bestSeller } = req.body
 
-        const image1 = req.files.image1[0]
-        const image2 = req.files.image1[0]
-        const image3 = req.files.image1[0]
-        const image4 = req.files.image1[0]
+        const image1 =req.files.image1 && req.files.image1[0]
+        const image2 =req.files.image2 && req.files.image2[0]
+        const image3 =req.files.image3 && req.files.image3[0]
+        const image4 =req.files.image4 && req.files.image4[0]
 
-        console.log(name, description, price, category, subCategory, sizes, bestSeller )
-        console.log(image1, image2, image3, image4)
+        const images = [image1, image2, image3, image4].filter((item)=>item !== undefined)
 
-        res.json({})
+        let imagesUrl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type: 'image'});
+                return result.secure_url
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestSeller: bestSeller=== "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        const product = new productModel(productData);
+        await product.save()
+
+        res.json({success:true, message:"Product Added"})
 
     }catch (error){
         console.log(error)
