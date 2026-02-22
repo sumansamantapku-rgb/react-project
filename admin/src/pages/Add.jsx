8554b9fa409205d52383c63b46265100186f4580
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-22 13:26:20 +0530

    fixed the blank  page after  reload

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <div>
+        <p>Upload Image</p>
+        <div>
+          <label htmlFor="image1">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden />
+          </label>
+        </div>
+      </div>
+    </form>
   )
 }
 
