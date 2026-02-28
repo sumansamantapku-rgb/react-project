e4ba0259332730d1be47eefba82d86895b54f60b
Suman Samanta <sumansamanta.pku@gmail.com>
2026-02-28 20:57:01 +0530

    fixed some minor issues in backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
@@ -18,6 +19,7 @@ app.use(cors())
 // api endpoints
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.get('/', (req, res)=>{
     res.send("API Working")
