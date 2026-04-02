8cfe6c873c41ee91b4b451703f69f98d88a696e1
Suman Samanta <sumansamanta.pku@gmail.com>
2026-04-02 10:09:21 +0530

    some minor changes

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
     subCategory: {type: String, required: true},
     sizes: {type: Array, required:true },
     bestseller: {type:Boolean},
-    data : {type:Number, required: true}
+    
 
 })
 
